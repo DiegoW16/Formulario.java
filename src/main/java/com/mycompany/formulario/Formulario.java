55855c91c701ac10ab25ea3b4ae5d42e8683e7d0
Jhojan <jmontanaa@unal.edu.co>
2022-05-04 23:34:38 -0500

    Estructura en Netbeans Sirviendo

---
+++ src/main/java/com/mycompany/formulario/Formulario.java
@@ -0,0 +1,75 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.formulario;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+/**
+ *
+ * @author jhojan
+ */
+
+public class Formulario extends JFrame implements ActionListener{
+	
+   private JMenuBar menubar;
+   private JMenu menu1;
+   private JMenuItem menuitem1, menuitem2, menuitem3;
+
+   public Formulario(){
+      setLayout(null);
+      menubar = new JMenuBar();
+      setJMenuBar(menubar);
+
+      menu1 = new JMenu("Opciones");
+      menubar.add(menu1);
+
+      menuitem1 = new JMenuItem("Rojo");
+      menuitem1.addActionListener(this);
+      menu1.add(menuitem1);
+      
+      menuitem2 = new JMenuItem("Verde");
+      menuitem2.addActionListener(this);
+      menu1.add(menuitem2);
+
+      menuitem3 = new JMenuItem("Azul");
+      menuitem3.addActionListener(this);
+      menu1.add(menuitem3);
+  }
+  
+      public void actionPerformed(ActionEvent e) {
+         Container fondo = this.getContentPane();
+
+       if(e.getSource() == menuitem1 ){
+		fondo.setBackground(new Color(225,0,0) );
+       }
+       if(e.getSource() == menuitem2){
+		fondo.setBackground(new Color(0,255,0) );
+       }
+       if(e.getSource() == menuitem3){
+		fondo.setBackground(new Color(0,0,255) );
+       }
+     }
+
+     public static void main(String args[]) {
+	  Formulario formulario1 = new Formulario();
+	  formulario1.setBounds(0,0,400,300);
+	  formulario1.setVisible(true);
+	  formulario1.setLocationRelativeTo(null);
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+}
